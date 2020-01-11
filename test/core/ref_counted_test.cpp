517d881bd43b0bf8039cd5ff0c80bb70e10afe54
Michael Beckemeyer <m.beckemeyer@gmx.net>
2020-01-11 21:01:06 +0100

    Test for Ref<T>.

--- test/core/ref_counted_test.cpp
+++ test/core/ref_counted_test.cpp
@@ -45,3 +45,91 @@ TEST_CASE(
     auto failed_lock = outer_weak.lock();
     REQUIRE_FALSE(failed_lock);
 }
+
+TEST_CASE("Ref counted objects should be destroyed if no longer referenced",
+    "[ref-counted]") {
+    int objects = 0;
+
+    struct TestClass : public RefCounted {
+        TestClass(int* counter)
+            : counter_(counter) {
+            ++*counter_;
+        }
+
+        ~TestClass() { --*counter_; }
+
+        int* counter_;
+    };
+
+    SECTION("Ref is destroyed") {
+        {
+            auto ref = make_ref<TestClass>(&objects);
+            REQUIRE(objects == 1);
+        }
+        REQUIRE(objects == 0);
+    }
+
+    SECTION("ref is reset") {
+        auto ref = make_ref<TestClass>(&objects);
+        REQUIRE(objects == 1);
+        ref.reset();
+        REQUIRE(objects == 0);
+    }
+
+    SECTION("ref is assigned") {
+        auto ref = make_ref<TestClass>(&objects);
+        REQUIRE(objects == 1);
+
+        auto ref2 = make_ref<TestClass>(&objects);
+        REQUIRE(objects == 2);
+
+        ref = ref2;
+        REQUIRE(objects == 1);
+
+        ref.reset();
+        REQUIRE(objects == 1);
+
+        ref2.reset();
+        REQUIRE(objects == 0);
+    }
+
+    SECTION("ref is move assigned") {
+        auto ref = make_ref<TestClass>(&objects);
+        REQUIRE(objects == 1);
+
+        auto ref2 = make_ref<TestClass>(&objects);
+        REQUIRE(objects == 2);
+
+        ref = std::move(ref2);
+        REQUIRE(objects == 1);
+        REQUIRE(!ref2);
+
+        ref.reset();
+        REQUIRE(objects == 0);
+    }
+
+    SECTION("ref is self assigned") {
+        auto ref = make_ref<TestClass>(&objects);
+        REQUIRE(objects == 1);
+
+        ref = ref;
+        REQUIRE(objects == 1);
+
+        ref.reset();
+        REQUIRE(objects == 0);
+    }
+
+    SECTION("ref is released") {
+        auto ref = make_ref<TestClass>(&objects);
+        REQUIRE(objects == 1);
+
+        auto ptr = ref.release();
+        REQUIRE(ptr);
+        REQUIRE(!ref);
+        REQUIRE(objects == 1);
+
+        ref = Ref(ptr, false);
+        ref.reset();
+        REQUIRE(objects == 0);
+    }
+}
