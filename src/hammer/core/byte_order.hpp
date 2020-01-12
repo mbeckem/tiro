a6d6dbcb337ab621a759d4797c1d26189492c5a3
Michael Beckemeyer <m.beckemeyer@gmx.net>
2020-01-12 16:13:02 +0100

    Make byte order implementation ready for windows (not tested yet).

--- src/hammer/core/byte_order.hpp
+++ src/hammer/core/byte_order.hpp
@@ -4,31 +4,71 @@
 #include "hammer/core/defs.hpp"
 #include "hammer/core/span.hpp"
 
+#ifdef _MSC_VER
+#    include <stdlib.h> // for byteswaps
+#endif
+
 namespace hammer {
 
 /// The possible values for the order of bytes within the binary representation of an integer.
 enum class ByteOrder {
-    BigEndian,
-    LittleEndian,
+    BigEndian,    // Most significant byte in lowest memory address
+    LittleEndian, // Most significant byte in highest memory address
 };
 
-// FIXME this is gcc specific
-#if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-inline constexpr byte_order host_byte_order = ByteOrder::BigEndian;
-#elif defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-inline constexpr ByteOrder host_byte_order = ByteOrder::LittleEndian;
+// __BYTE_ORDER__ is gcc
+#if defined(__BYTE_ORDER__)
+#    if defined(__ORDER_BIG_ENDIAN__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#        define HAMMER_BYTE_ORDER ByteOrder::BigEndian
+#    elif defined(__ORDER_LITTLE_ENDIAN__) \
+        && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#        define HAMMER_BYTE_ORDER ByteOrder::LittleEndian
+#    else
+#        error Unsupported byte order.
+#    endif
+// __{*}_ENDIAN__ is clang
+#elif defined(__BIG_ENDIAN__)
+#    define HAMMER_BYTE_ORDER ByteOrder::BigEndian
+#elif defined(__LITTLE_ENDIAN__)
+#    define HAMMER_BYTE_ORDER ByteOrder::LittleEndian
+// This should cover win32 and any other x86 machines
+#elif defined(_MSC_VER) || defined(__i386__) || defined(__x86_64__)
+#    define HAMMER_BYTE_ORDER ByteOrder::LittleEndian
+#else
+#    error Failed to detect byte order.
 #endif
 
+/// Holds the native byte order of the host.
+inline constexpr ByteOrder host_byte_order = HAMMER_BYTE_ORDER;
+
 constexpr u16 byteswap(u16 v) {
+#if defined(__GNUC__) || defined(__clang__)
     return __builtin_bswap16(v);
+#elif defined(_MSC_VER)
+    return _byteswap_ushort(v);
+#else
+#    error No byteswap implementation for this platform as of yet.
+#endif
 }
 
 constexpr u32 byteswap(u32 v) {
+#if defined(__GNUC__) || defined(__clang__)
     return __builtin_bswap32(v);
+#elif defined(_MSC_VER)
+    return _byteswap_ulong(v);
+#else
+#    error No byteswap implementation for this platform as of yet.
+#endif
 }
 
 constexpr u64 byteswap(u64 v) {
+#if defined(__GNUC__) || defined(__clang__)
     return __builtin_bswap64(v);
+#elif defined(_MSC_VER)
+    return _byteswap_uint64(v);
+#else
+#    error No byteswap implementation for this platform as of yet.
+#endif
 }
 
 template<typename T>
