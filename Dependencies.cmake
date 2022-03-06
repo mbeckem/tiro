e286e34813add5d4732b2efb5adb4fddd130276f
Michael Beckemeyer <m.beckemeyer@gmx.net>
2022-03-06 15:22:26 +0100

    Reenable static builds.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     abseil_cpp
     GIT_REPOSITORY  https://github.com/abseil/abseil-cpp.git
-    GIT_TAG         384af0e9141283172e2bff3210dae79fb7130d9c
+    GIT_TAG         04bde89e5cb33bf4a714a5496fac715481fc4831
     GIT_PROGRESS    TRUE
     #GIT_SHALLOW     TRUE # (does not work for specific commit hashes on github)
 )
@@ -58,25 +58,45 @@ endif()
 # set(FETCHCONTENT_FULLY_DISCONNECTED TRUE)
 # set(FETCHCONTENT_UPDATES_DISCONNECTED TRUE)
 
+# When building as a static library, we must enable the install targets
+# of runtime dependencies. This means that the subdirectories must
+# also be added without EXCLUDE_FROM_ALL, making builds slower.
+set(INSTALL_DEPENDENCIES OFF)
+if (NOT ${TIRO_BUILD_SHARED})
+    set(INSTALL_DEPENDENCIES ON)
+endif()
+
+function(_add_dependency_subdir source_dir dest_dir)
+    set(_mode "EXCLUDE_FROM_ALL")
+    if (INSTALL_DEPENDENCIES)
+        set(_mode "")
+    endif()
+
+    add_subdirectory("${source_dir}" "${dest_dir}" ${_mode})
+endfunction()
+
 FetchContent_GetProperties(abseil_cpp)
 if(NOT abseil_cpp_POPULATED)
     message(STATUS "### Dependency: abseil_cpp")
+    set(ABSL_PROPAGATE_CXX_STD ON CACHE BOOL "-- Override --" FORCE)
+    set(ABSL_ENABLE_INSTALL ${INSTALL_DEPENDENCIES} CACHE BOOL "-- Override --" FORCE)
     FetchContent_Populate(abseil_cpp)
-    add_subdirectory(${abseil_cpp_SOURCE_DIR} ${abseil_cpp_BINARY_DIR} EXCLUDE_FROM_ALL)
+    _add_dependency_subdir(${abseil_cpp_SOURCE_DIR} ${abseil_cpp_BINARY_DIR})
 endif()
 
 FetchContent_GetProperties(catch2)
 if(NOT catch2_POPULATED)
     message(STATUS "### Dependency: catch2")
     FetchContent_Populate(catch2)
-    add_subdirectory(${catch2_SOURCE_DIR} ${catch2_BINARY_DIR} EXCLUDE_FROM_ALL)
+    add_subdirectory(${catch2_SOURCE_DIR} ${catch2_BINARY_DIR})
 endif()
 
 FetchContent_GetProperties(fmt)
 if(NOT fmt_POPULATED)
     message(STATUS "### Dependency: fmt")
     FetchContent_Populate(fmt)
-    add_subdirectory(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR} EXCLUDE_FROM_ALL)
+    set(FMT_INSTALL ${INSTALL_DEPENDENCIES} CACHE BOOL "-- Override --" FORCE)
+    _add_dependency_subdir(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR})
 endif()
 
 FetchContent_GetProperties(nlohmann_json)
@@ -84,7 +104,7 @@ if(NOT nlohmann_json_POPULATED)
     message(STATUS "### Dependency: nlohmann_json")
     FetchContent_Populate(nlohmann_json)
     set(JSON_BuildTests OFF CACHE BOOL "--Override --" FORCE)
-    add_subdirectory(${nlohmann_json_SOURCE_DIR} ${nlohmann_json_BINARY_DIR} EXCLUDE_FROM_ALL)
+    _add_dependency_subdir(${nlohmann_json_SOURCE_DIR} ${nlohmann_json_BINARY_DIR})
 endif()
 
 FetchContent_GetProperties(utfcpp)
@@ -92,9 +112,9 @@ if(NOT utfcpp_POPULATED)
     message(STATUS "### Dependency: utfcpp")
     FetchContent_Populate(utfcpp)
     set(UTF8_TESTS OFF CACHE BOOL "-- Override --" FORCE)
-    set(UTF8_INSTALL OFF CACHE BOOL "-- Override --" FORCE)
+    set(UTF8_INSTALL ${INSTALL_DEPENDENCIES} CACHE BOOL "-- Override --" FORCE)
     set(UTF8_SAMPLES OFF CACHE BOOL "-- Override --" FORCE)
-    add_subdirectory(${utfcpp_SOURCE_DIR} ${utfcpp_BINARY_DIR} EXCLUDE_FROM_ALL)
+    _add_dependency_subdir(${utfcpp_SOURCE_DIR} ${utfcpp_BINARY_DIR})
 endif()
 
 FetchContent_GetProperties(cxxopts)
@@ -103,5 +123,5 @@ if(NOT cxxopts_POPULATED)
     set(CXXOPTS_BUILD_EXAMPLES OFF CACHE BOOL "-- Override --" FORCE)
     set(CXXOPTS_BUILD_TESTS OFF CACHE BOOL "-- Override --" FORCE)
     FetchContent_Populate(cxxopts)
-    add_subdirectory(${cxxopts_SOURCE_DIR} ${cxxopts_BINARY_DIR} EXCLUDE_FROM_ALL)
+    add_subdirectory(${cxxopts_SOURCE_DIR} ${cxxopts_BINARY_DIR})
 endif()
